ed7215fce0f978f478f5efa9e4b1591c7b18dc66
Maike <Maike.Fillmer@gmail.com>
2016-11-27 22:19:01 -0500

    first commit

---
+++ controller.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+module.exports = function(app){
+	app.get('/', function(req,res) {
+        res.render('index');
+        
+    });
+    app.get('/quiz', function(req,res) {
+        res.render('quiz');
+        
+    });
+}
